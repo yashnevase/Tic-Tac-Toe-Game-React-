126ea0a8610b56b4da05128ac2f05441ddf496f5
yashnevase <78201930+yashnevase@users.noreply.github.com>
2024-09-23 17:25:59 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import  { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const handleClick = (index) => {
+    const newBoard = [...board];
+    if (calculateWinner(board) || newBoard[index]) return;
+    newBoard[index] = isXNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
+
+  const winner = calculateWinner(board);
+  const status = winner ? `Winner: ${winner}` : `Next player: ${isXNext ? 'X' : 'O'}`;
+  const winnerMessage = winner ? `Congratulations! ${winner} is the winner!` : '';
+
+  return (
+    <div className="game">
+      <div className="winner-message">{winnerMessage}</div>
+      <div className="game-board">
+        {board.map((value, index) => (
+          <button key={index} className="square" onClick={() => handleClick(index)} data-value={value}>
+            {value}
+          </button>
+        ))}
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <button onClick={() => setBoard(Array(9).fill(null))} ><div className="word1">Restart</div></button>
+      </div>
+    </div>
+    
+
+  );
+}
+
+function calculateWinner(board) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+  return null;
+}
+
+export default App;
